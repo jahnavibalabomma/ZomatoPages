a96724edc33d9098c9064b801434418a043d125c
Jahnavi.B <139149353+jahnavibalabomma@users.noreply.github.com>
2023-09-19 16:14:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PubsBarPage from "./PagesOfZomato/PubsBarPage";
+
+function App() {
+  return (
+    <div>
+      <PubsBarPage></PubsBarPage>
+    </div>
+  );
+}
+
+export default App;
